7c8e3f971db552764fb3ba26247c8ec02b56e0db
Nicholas Jackson <nicdogg360@gmail.com>
2020-12-08 13:26:24 -0800

    some more touches

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,8 @@ let arrows_d = document.querySelectorAll(".arrow-d");
 let header = document.querySelectorAll(".text-block-header");
 let text = document.querySelectorAll(".text-block-body");
 
-console.log(arrows_u);
-console.log(header);
 
-
+// function definitions
 let hide = () => {
     for(let i = 0; i < arrows_u.length; i++){
         arrows_u[i].addEventListener("click", function () {
@@ -21,7 +19,6 @@ let hide = () => {
         })
     }
 } 
-
 let show = () => {
     for(let i = 0; i < arrows_d.length; i++){
         arrows_d[i].addEventListener("click", function () {
